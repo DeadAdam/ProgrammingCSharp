15b465fdcc81938c7fd64c4b98362049c9beba93
DeadAdam <55098583+DeadAdam@users.noreply.github.com>
2019-09-17 00:13:34 +0300

    Add files via upload

---
+++ Lab1/1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+
+namespace _1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           double t;
+            const double g = 9.83;
+            double h;
+            double V;
+
+            Console.WriteLine("Enter Height: ");
+             h =  Convert.ToDouble(Console.ReadLine());
+           Console.WriteLine("Enter current speed: ");
+            V = Convert.ToDouble(Console.ReadLine());
+            t = Convert.ToDouble(Math.Sqrt((2 * (h - V)) / g));
+
+            
+            Console.WriteLine("Time of falling: " + t);
+
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+
+    }
+}
---
+++ Lab2/2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace _2
+{
+    class Program
+    {
+        static void Main()
+        {
+            double k,n,a,N;
+
+            Console.WriteLine("Enter n:");
+
+            do
+            {
+                n = Convert.ToDouble(Console.ReadLine());
+                if (n < 0)
+                 {
+                    Console.WriteLine("Error");
+                 }
+            } while (n < 0);
+
+            k = n * n;
+            N = n * k;
+            a = 0;
+
+            for (; k <= N; k++)
+            {
+                a = Convert.ToDouble( a + (((Math.Pow(k,2)- (Math.Pow(-1, Math.Pow(k,2)+1))*k))/(Math.Pow(k,2)+2)));
+
+            }
+
+            Console.WriteLine("S = " + a);
+
+            Console.WriteLine();
+            Console.ReadLine();
+
+        }
+
+
+
+    }
+
+}
